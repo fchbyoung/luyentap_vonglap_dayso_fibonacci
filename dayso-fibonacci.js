32040020956e4a02cc76e2e37e1343c38cca5673
Long <fchbyoung1010@gmail.com>
2019-06-03 11:36:53 +0700

    fibonacci

--- dayso-fibonacci.js
+++ dayso-fibonacci.js
@@ -1,17 +1,19 @@
     let a = 0; b = 1;
     let temp;
     let check = false;
+    let sum = 0;
     for(let i = 0; i < 20; i++){
         temp = a;
         a += b;
         b = temp;
+        sum += a;
         document.writeln(a);
         if(a % 5 === 0 && !check){
             check = a;
         }
     }
     document.write("<br/>" + "Số đầu tiên chia hết cho 5 là : " + check);
-
+    document.write("<br/>" + "tổng 20 số đầu tiên trong dãy fibonacci là : " + sum);
 
 
 
