2f9128a53e1b3cba7999fcbc71f2483ed5c86162
Long <fchbyoung1010@gmail.com>
2019-06-03 11:29:10 +0700

    fibonacci

--- dayso-fibonacci.js
+++ dayso-fibonacci.js
@@ -1,10 +1,27 @@
     let a = 0; b = 1;
     let temp;
+    let check = false;
     for(let i = 0; i < 20; i++){
         temp = a;
         a += b;
         b = temp;
         document.writeln(a);
+        if(a % 5 === 0 && !check){
+            check = a;
+        }
     }
+    document.write("<br/>" + "Số đầu tiên chia hết cho 5 là : " + check);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
